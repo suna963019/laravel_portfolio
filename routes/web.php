1c01c999f3e4f5cd38c318fa0b9e72d27fbc2748
suna963019 <sunahara20011205@gmail.com>
2024-03-11 09:17:58 +0900

    route設定

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\RakutenAPIController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,13 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[BookController::class,'index']);
-
-// Route::get('/rakuten','BookController@rakuten');
-
-
-// Route::get('/rakuten/add','BookController@rakutenAdd');
-// Route::post('/rakuten/add','BookController@rakutenCreate');
-
 Route::get('/book',[BookController::class,'index']);
 
 Route::get('/book/add',[BookController::class,'add']);
@@ -35,3 +29,9 @@ Route::get('/book/del',[BookController::class,'delete']);
 Route::post('/book/del',[BookController::class,'remove']);
 
 Route::get('/book/search',[BookController::class,'search']);
+
+//楽天API用
+Route::get('/rakuten',[RakutenAPIController::class,'']);
+
+Route::get('/rakuten/add',[RakutenAPIController::class,'']);
+Route::post('/rakuten/add',[RakutenAPIController::class,'']);
